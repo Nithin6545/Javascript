98827db6769c562acfc6e23b9e84895500c07c29
nithin <nithin6545reddy@gmail.com>
2024-11-19 14:01:08 +0530

    DOM

--- calc.js
+++ calc.js
@@ -9,10 +9,26 @@ for(let i=0;i<y.length;i++)
                 inp.value += y[i].innerHTML  //to concat in input display
             })
         }
-
-    function ac(){
-        
-        document.getElementById('inp').value = ""
-        
+function ac(){
+    inp.value = '';
     }
+function ce(){
+    inp.value =  inp.value.slice(0,-1)
+}
 
+// let a = document.getElementById("ce")
+// console.log(a);
+// a.addEventListener("click",()=>{
+//     let l = inp.value
+//     inp.value=l.slice(0, -1)
+// })
+
+// let res = document.getElementsById("eq")
+// console.log(res);
+// res.addEventListener("click",()=>{
+//     inp.value=eval(inp.value)
+// })
+
+function eq(){
+    inp.value=eval(inp.value)
+}
---
+++ print.js
@@ -0,0 +1,78 @@
+// function f1(){
+//     let data = document.getElementById("i").value
+//     document.getElementById("h").innerHTML=data
+// }
+// function f2(){
+//     let c = document.getElementById("i").value
+//     let a = document.getElementById("eagle")
+//     let b = document.getElementById("hh").innerHTML=c
+// }
+// function f3(){
+//     let a = document.getElementById("eagle")
+//     let b = document.getElementById("hh").innerHTML=("")
+// }
+
+
+// let z = document.querySelector("h2")
+// z.style.color='red'
+// z.style.fontSize='50px'
+
+// z.style.cssText="text-align:center; color:blue; font-size:20px "
+
+// function background(){
+//     let input = document.getElementById("i2").value
+//     let bc = document.querySelector("body")
+//     bc.style.backgroundColor=input
+// }
+
+
+// let abc = document.getElementById('hello')
+// //console.log(inp);
+// let y = document.getElementsByClassName("bt")
+// // console.log(y);
+
+// for(let i=0;i<y.length;i++)
+//     {
+//         y[i].addEventListener("click",()=>
+//         {
+//             abc.style.color = y[i].value  //can do directly using innerHTML
+            
+//         })
+//     }
+
+
+
+    
+    // function changeColor(color) 
+    // { document.getElementById('hello').style.color = color;}
+
+    // function f2()
+    // {
+    //     document.getElementById('ha').style.color = document.getElementById('ha').style.color === 'red' ? 'blue' : 'red';;
+    // }
+
+    // function f3()
+    // {
+    //     let cl = document.getElementById('ha');
+    //     if(cl.style.color === 'green')
+    //         cl.style.color = 'red'
+    //     else
+    //     cl.style.color = 'green'
+    // }
+
+    function f4()
+    {
+        let y = document.getElementById("ran").value
+        document.getElementById("r").style.fontSize=y+"px"
+    }
+
+    function f5()
+    {
+        let h = document.getElementById("h").value
+        let w = document.getElementById("w").value
+        document.querySelector("img").style.height=h+"px"
+        document.querySelector("img").style.width=w+"px"
+    }
+
+ 
+    
